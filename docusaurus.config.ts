78d7eb834e1faa86b3d555a61809dc1870a0b235
“Mpho <em.magoro@gmail.com>
2026-09-09 21:23:17 +0100

    fix: update organization and project name in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,14 +11,13 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://mphomagoro.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MPHOMAGORO', 
+  projectName: 'mphomagoro-on-tech', 
 
   onBrokenLinks: 'throw',
 
